3763b5615514c6c37c7e6f75c38d12014a1523c1
Tomáš Kolinger <tomas@kolinger.name>
2022-08-19 11:18:26 +0200

    download.py: refactoring

--- download.py
+++ download.py
@@ -1,131 +1,127 @@
 #!/usr/bin/env python3
+import argparse
+import logging
+from logging import StreamHandler
+import os
 import platform
 import shlex
 import subprocess
+import sys
+from timeit import default_timer as timer
 
-try:
-    import argparse
-    import logging
-    from logging import FileHandler, StreamHandler
-    import os
-    import sys
-    from timeit import default_timer as timer
+from deployment.config import Config
+from deployment.exceptions import MessageException
 
-    from deployment.config import Config
-    from deployment.deployment import Deployment
-    from deployment.exceptions import MessageException
+known_operations = [
+    "Finished mirror",
+    "Finished transfer",
+    "Transferring file",
+    "Removing old file",
+    "Making directory",
+    "Removing old directory",
+]
 
-    known_operations = [
-        "Finished mirror",
-        "Finished transfer",
-        "Transferring file",
-        "Removing old file",
-        "Making directory",
-        "Removing old directory",
+
+def mirror(config, force, lftp_binary):
+    parameters = "-vv"
+    for path in config.ignore:
+        parameters += " --exclude %s" % path
+    if force:
+        parameters += " --continue"
+
+    execute = [
+        "set net:connection-limit %s" % config.threads,
+        "set net:timeout 30",
+        "set net:max-retries 20",
+        "set dns:order \\\"inet inet6\\\"",
+        "set mirror:parallel-directories true",
     ]
 
+    error_wait = config.connection_limit_wait
+    if error_wait > 0:
+        execute.append("set net:reconnect-interval-base %s" % error_wait)
+        execute.append("set net:reconnect-interval-max %s" % error_wait)
+        execute.append("set net:reconnect-interval-multiplier 1")
 
-    def mirror(config, force, lftp_binary):
-        parameters = "-vv"
-        for path in config.ignore:
-            parameters += " --exclude %s" % path
-        if force:
-            parameters += " --continue"
+    if config.bind:
+        execute.append("set net:socket-bind-ipv4 %s" % config.bind)
 
-        execute = [
-            "set net:connection-limit %s" % config.threads,
-            "set net:timeout 30",
-            "set net:max-retries 20",
-            "set dns:order \\\"inet inet6\\\"",
-            "set mirror:parallel-directories true",
-        ]
+    if config.secure:
+        execute.append("set ssl:verify-certificate false")
+    else:
+        execute.append("set ftp:ssl-allow false")
 
-        error_wait = config.connection_limit_wait
-        if error_wait > 0:
-            execute.append("set net:reconnect-interval-base %s" % error_wait)
-            execute.append("set net:reconnect-interval-max %s" % error_wait)
-            execute.append("set net:reconnect-interval-multiplier 1")
+    execute.extend([
+        "open %s:%s" % (config.host, config.port),
+        "user %s %s" % (shlex.quote(config.user), shlex.quote(config.password)),
+        "mirror --delete --parallel=%s %s %s" % (
+            config.threads, parameters, config.remote
+        ),
+        "echo done",
+    ])
 
-        if config.bind:
-            execute.append("set net:socket-bind-ipv4 %s" % config.bind)
+    if platform.system() == "Windows":
+        lftp_binary = "wsl.exe %s" % lftp_binary
 
-        if config.secure:
-            execute.append("set ssl:verify-certificate false")
+    command = [
+        "cd %s" % config.local,
+        "%s -c \"%s\"" % (lftp_binary, " && ".join(execute)),
+    ]
+    command = " && ".join(command)
+
+    lines = []
+    process = subprocess.Popen(command, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+    while process.poll() is None:
+        for line in process.stdout:
+            line = line.decode("utf-8", errors="ignore").rstrip()
+            print(line)
+            lines.append(line)
+
+    unexpected = []
+    expected = 0
+    found_done = False
+    for line in lines:
+        is_known = False
+        for known in known_operations:
+            if line.startswith(known):
+                is_known = True
+                break
+
+        if is_known:
+            expected += 1
         else:
-            execute.append("set ftp:ssl-allow false")
-
-        execute.extend([
-            "open %s:%s" % (config.host, config.port),
-            "user %s %s" % (shlex.quote(config.user), shlex.quote(config.password)),
-            "mirror --delete --parallel=%s %s %s" % (
-                config.threads, parameters, config.remote
-            ),
-            "echo done",
-        ])
-
-        if platform.system() == "Windows":
-            lftp_binary = "wsl.exe %s" % lftp_binary
-
-        command = [
-            "cd %s" % config.local,
-            "%s -c \"%s\"" % (lftp_binary, " && ".join(execute)),
-        ]
-        command = " && ".join(command)
-
-        lines = []
-        process = subprocess.Popen(command, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-        while process.poll() is None:
-            for line in process.stdout:
-                line = line.decode("utf-8", errors="ignore").rstrip()
-                print(line)
-                lines.append(line)
-
-        unexpected = []
-        expected = 0
-        found_done = False
-        for line in lines:
-            is_known = False
-            for known in known_operations:
-                if line.startswith(known):
-                    is_known = True
-                    break
-
-            if is_known:
-                expected += 1
+            if line == "done":
+                found_done = True
             else:
-                if line == "done":
-                    found_done = True
-                else:
-                    unexpected.append(line)
+                unexpected.append(line)
 
-        if process.returncode != 0:
-            safe = command.replace(config.password, "****")
-            logging.error("lftp mirror failed with command: %s, exit code: %s, unexpected output: %s" % (
-                safe, process.returncode, "\n".join(unexpected)
-            ))
-            exit(1)
+    if process.returncode != 0:
+        safe = command.replace(config.password, "****")
+        logging.error("lftp mirror failed with command: %s, exit code: %s, unexpected output: %s" % (
+            safe, process.returncode, "\n".join(unexpected)
+        ))
+        exit(1)
 
-        if (len(unexpected) > 0 or expected == 0) and found_done:
-            safe = command.replace(config.password, "****")
-            logging.error("lftp mirror returned unexpected output with command: %s, unexpected output: %s" % (
-                safe, "\n".join(unexpected)
-            ))
-            exit(1)
+    if (len(unexpected) > 0 or expected == 0) and found_done:
+        safe = command.replace(config.password, "****")
+        logging.error("lftp mirror returned unexpected output with command: %s, unexpected output: %s" % (
+            safe, "\n".join(unexpected)
+        ))
+        exit(1)
 
-        logging.info("Completed")
+    logging.info("Completed")
 
 
-    if __name__ == "__main__":
-        logger = logging.getLogger()
-        logger.setLevel(logging.DEBUG)
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
 
-        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    console = StreamHandler()
+    console.setLevel(logging.INFO)
+    console.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+    logger.addHandler(console)
 
-        console = StreamHandler()
-        console.setLevel(logging.DEBUG)
-        console.setFormatter(formatter)
-        logger.addHandler(console)
-
+    try:
         parser = argparse.ArgumentParser(add_help=False)
         parser.add_argument("--help", action="help")
         parser.add_argument("-h", "--host", help="FTP host (port can be specified with colon)", required=True)
@@ -145,62 +141,58 @@ try:
         parser.add_argument("--lftp-binary", help="lftp binary path", default="/usr/bin/lftp")
         args = parser.parse_args()
 
-        downloader = None
-        try:
-            config = Config()
+        config = Config()
 
-            config.host = args.host
-            if ":" in args.host:
-                host, port = args.host.split(":")
-                try:
-                    config.port = int(port)
-                    config.host = host
-                except ValueError:
-                    config.host = args.host
+        config.host = args.host
+        if ":" in args.host:
+            host, port = args.host.split(":")
+            try:
+                config.port = int(port)
+                config.host = host
+            except ValueError:
+                config.host = args.host
 
-            config.user = args.user
+        config.user = args.user
 
-            if args.password is not None:
-                config.password = args.password
-            else:
-                config.password = input("Password: ")
+        if args.password is not None:
+            config.password = args.password
+        else:
+            config.password = input("Password: ")
 
-            config.secure = not args.insecure
-            # config.implicit = args.implicit
-            # config.passive = not args.active
-            # config.passive_workaround = args.passive_workaround
-            config.connection_limit_wait = args.error_wait
-            config.threads = args.threads
-            config.bind = args.bind
+        config.secure = not args.insecure
+        # config.implicit = args.implicit
+        # config.passive = not args.active
+        # config.passive_workaround = args.passive_workaround
+        config.connection_limit_wait = args.error_wait
+        config.threads = args.threads
+        config.bind = args.bind
 
-            config.remote = args.remote
-            config.local = args.local if args.local != "." else os.getcwd()
+        config.remote = args.remote
+        config.local = args.local if args.local != "." else os.getcwd()
 
-            if args.ignore:
-                config.ignore = args.ignore
+        if args.ignore:
+            config.ignore = args.ignore
 
-            start_time = timer()
-            logging.info("Downloading %s@%s%s" % (config.user, config.host, config.remote))
-            logging.info("Using %s threads" % config.threads)
+        start_time = timer()
+        logging.info("Downloading %s@%s%s" % (config.user, config.host, config.remote))
+        logging.info("Using %s threads" % config.threads)
 
-            mirror(config, force=args.force, lftp_binary=args.lftp_binary)
+        mirror(config, force=args.force, lftp_binary=args.lftp_binary)
 
-            logging.info("Elapsed time %s seconds" % (timer() - start_time))
+        logging.info("Elapsed time %s seconds" % (timer() - start_time))
 
-            sys.exit(0)
+        exit(0)
 
-        except MessageException as e:
-            logging.error(str(e))
-            sys.exit(1)
-        except SystemExit as e:
-            if e.code != 0:
-                logging.critical("Terminated with code %s" % e.code)
-        except KeyboardInterrupt:
-            logging.critical("Terminated by user")
-            sys.exit(1)
-        except:
-            logging.exception(sys.exc_info()[0])
-            sys.exit(1)
-
-except (KeyboardInterrupt, SystemExit):
-    exit(1)
+    except MessageException as e:
+        logging.error(str(e))
+        exit(1)
+    except SystemExit as e:
+        if e.code != 0:
+            logging.critical("Terminated with code %s" % e.code)
+            exit(e.code)
+    except KeyboardInterrupt:
+        logging.critical("Terminated by user")
+        exit(1)
+    except:
+        logging.exception(sys.exc_info()[0])
+        exit(1)
